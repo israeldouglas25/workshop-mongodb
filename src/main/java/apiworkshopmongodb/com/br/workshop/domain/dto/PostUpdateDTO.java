89955250658d3830a9838f8066a159cc01234d84
israel.paixao <israeldouglas25@gmail.com>
2025-10-28 16:57:54 -0300

    created class update post

---
+++ src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/PostUpdateDTO.java
@@ -0,0 +1,12 @@
+package apiworkshopmongodb.com.br.workshop.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostUpdateDTO {
+
+    private String title;
+    private String body;
+}
