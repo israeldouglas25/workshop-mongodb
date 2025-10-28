b951d1ff74fc0dd9398fd72a241de8c9d8fda52b
israel.paixao <israeldouglas25@gmail.com>
2025-10-28 18:09:03 -0300

    created new type list

---
+++ src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/PostResponseDTO.java
@@ -0,0 +1,11 @@
+package apiworkshopmongodb.com.br.workshop.domain.dto;
+
+import apiworkshopmongodb.com.br.workshop.domain.Post;
+
+import java.time.LocalDate;
+
+public record PostResponseDTO(String id, LocalDate date, String title, String body) {
+    public PostResponseDTO (Post post) {
+        this(post.getId(), post.getDate(), post.getTitle(), post.getBody());
+    }
+}
