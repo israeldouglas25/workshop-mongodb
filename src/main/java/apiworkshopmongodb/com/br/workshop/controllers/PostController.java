aea2cfb94a9a9cf8ca2497b44a3490ca2eaee3af
israel.paixao <israeldouglas25@gmail.com>
2025-10-28 16:56:01 -0300

    created CRUD all Post

--- src/main/java/apiworkshopmongodb/com/br/workshop/controllers/PostController.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/controllers/PostController.java
@@ -2,6 +2,7 @@ package apiworkshopmongodb.com.br.workshop.controllers;
 
 import apiworkshopmongodb.com.br.workshop.domain.Post;
 import apiworkshopmongodb.com.br.workshop.domain.dto.PostDTO;
+import apiworkshopmongodb.com.br.workshop.domain.dto.PostUpdateDTO;
 import apiworkshopmongodb.com.br.workshop.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +24,28 @@ public class PostController {
         return ResponseEntity.ok(postService.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        return ResponseEntity.ok(postService.findById(id));
+    }
+
     @PostMapping
     public ResponseEntity<Post> create(@RequestBody PostDTO dto) {
         Post postd = postService.create(dto);
         URI uri = UriComponentsBuilder.fromPath("/posts/{id}").buildAndExpand(postd.getId()).toUri();
         return ResponseEntity.created(uri).body(postd);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        postService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody PostUpdateDTO dto) {
+        Post updatedPost = postService.update(id, dto);
+        return ResponseEntity.ok(updatedPost);
+    }
+
 }
--- src/main/java/apiworkshopmongodb/com/br/workshop/services/PostService.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/services/PostService.java
@@ -3,7 +3,9 @@ package apiworkshopmongodb.com.br.workshop.services;
 import apiworkshopmongodb.com.br.workshop.domain.Post;
 import apiworkshopmongodb.com.br.workshop.domain.User;
 import apiworkshopmongodb.com.br.workshop.domain.dto.PostDTO;
+import apiworkshopmongodb.com.br.workshop.domain.dto.PostUpdateDTO;
 import apiworkshopmongodb.com.br.workshop.domain.dto.UserPostResponseDTO;
+import apiworkshopmongodb.com.br.workshop.exceptions.NotFoundException;
 import apiworkshopmongodb.com.br.workshop.interfaces.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,10 @@ public class PostService {
         return postRepository.findAll();
     }
 
+    public Post findById(String id) {
+        return postRepository.findById(id).orElseThrow(() -> new NotFoundException("Post not found"));
+    }
+
     public Post create(PostDTO dto) {
         User user = userService.findById(dto.getIdAuthor());
         UserPostResponseDTO author = new UserPostResponseDTO(user);
@@ -35,6 +41,24 @@ public class PostService {
     }
 
     public void delete(String id) {
-        postRepository.deleteById(id);
+        Post existingPost = findById(id);
+        postRepository.deleteById(existingPost.getId());
+    }
+
+    public Post update(String id, PostUpdateDTO dto) {
+        Post existingPost = findById(id);
+        updateData(existingPost, dto);
+        return postRepository.save(existingPost);
+    }
+
+    private void updateData(Post existingPost, PostUpdateDTO dto) {
+        if (dto.getTitle() != null && !dto.getTitle().isEmpty()) {
+            existingPost.setDate(LocalDate.now());
+            existingPost.setTitle(dto.getTitle());
+        }
+        if (dto.getBody() != null && !dto.getBody().isEmpty()) {
+            existingPost.setDate(LocalDate.now());
+            existingPost.setBody(dto.getBody());
+        }
     }
 }
