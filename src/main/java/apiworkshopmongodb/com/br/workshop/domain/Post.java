a8355ff5f836e927393f884d3585ee152b2c4fff
israel.paixao <israeldouglas25@gmail.com>
2025-10-29 11:36:48 -0300

    include list comment in post

--- src/main/java/apiworkshopmongodb/com/br/workshop/domain/Post.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/domain/Post.java
@@ -1,12 +1,19 @@
 package apiworkshopmongodb.com.br.workshop.domain;
 
 import apiworkshopmongodb.com.br.workshop.domain.dto.AuthorDTO;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
+@Document
 @NoArgsConstructor
 @Getter
 @Setter
@@ -20,4 +27,15 @@ public class Post {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    @DBRef(lazy = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
