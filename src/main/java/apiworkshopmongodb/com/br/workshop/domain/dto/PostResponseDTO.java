d28b4de53ee5f740be1147e511345414c12314fb
israel.paixao <israeldouglas25@gmail.com>
2025-10-29 11:41:15 -0300

    Adjusting post response formatting

--- src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/PostResponseDTO.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/PostResponseDTO.java
@@ -5,7 +5,7 @@ import apiworkshopmongodb.com.br.workshop.domain.Post;
 import java.time.LocalDate;
 
 public record PostResponseDTO(String id, LocalDate date, String title, String body) {
-    public PostResponseDTO (Post post) {
+    public PostResponseDTO(Post post) {
         this(post.getId(), post.getDate(), post.getTitle(), post.getBody());
     }
 }
--- src/main/java/apiworkshopmongodb/com/br/workshop/services/PostService.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/services/PostService.java
@@ -2,10 +2,9 @@ package apiworkshopmongodb.com.br.workshop.services;
 
 import apiworkshopmongodb.com.br.workshop.domain.Post;
 import apiworkshopmongodb.com.br.workshop.domain.User;
-import apiworkshopmongodb.com.br.workshop.domain.dto.PostDTO;
-import apiworkshopmongodb.com.br.workshop.domain.dto.PostResponseDTO;
-import apiworkshopmongodb.com.br.workshop.domain.dto.PostUpdateDTO;
 import apiworkshopmongodb.com.br.workshop.domain.dto.AuthorDTO;
+import apiworkshopmongodb.com.br.workshop.domain.dto.PostDTO;
+import apiworkshopmongodb.com.br.workshop.domain.dto.PostUpdateDTO;
 import apiworkshopmongodb.com.br.workshop.exceptions.NotFoundException;
 import apiworkshopmongodb.com.br.workshop.interfaces.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +35,7 @@ public class PostService {
         AuthorDTO author = new AuthorDTO(user);
         Post post = new Post(null, LocalDate.now(), dto.getTitle(), dto.getBody(), author);
         Post postSave = postRepository.save(post);
-        user.getPosts().add(new PostResponseDTO(postSave));
+        user.getPosts().add(postSave);
         userService.savePost(user);
         return postSave;
     }
