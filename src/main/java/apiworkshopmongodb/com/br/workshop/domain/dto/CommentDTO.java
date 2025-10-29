0dc9ccfbd495a24a28fda37ce7eb08425412271a
israel.paixao <israeldouglas25@gmail.com>
2025-10-29 14:25:07 -0300

    Change record to class

--- src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/CommentDTO.java
+++ src/main/java/apiworkshopmongodb/com/br/workshop/domain/dto/CommentDTO.java
@@ -1,16 +1,13 @@
 package apiworkshopmongodb.com.br.workshop.domain.dto;
 
-import apiworkshopmongodb.com.br.workshop.domain.Comment;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
+@Getter
+@Setter
+public class CommentDTO {
 
-public record CommentDTO(
-        String text,
-        LocalDate date,
-        AuthorDTO author) {
-
-    public CommentDTO(Comment comment) {
-        this(comment.getText(), comment.getDate(), comment.getAuthor());
-    }
+    private String text;
+    private String author;
 
 }
